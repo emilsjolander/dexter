30196fff2f9d1b9dc142287b700cfddfe91dfedc
Emil Sjölander <sjolander.emil@gmail.com>
2014-12-21 23:43:57 +0100

    Remove profiling code

--- knn/example/main.go
+++ knn/example/main.go
@@ -2,29 +2,14 @@ package main
 
 import (
 	"encoding/csv"
-	"flag"
 	"fmt"
-	"log"
 	"os"
-	"runtime/pprof"
 	"strconv"
 
 	"github.com/emilsjolander/dexter/knn"
 )
 
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
-
 func main() {
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pprof.StartCPUProfile(f)
-		defer pprof.StopCPUProfile()
-	}
-
 	trainingInputs, trainingClasses := readData("optdigits.tra")
 	testingInputs, testingClasses := readData("optdigits.tes")
 
